53f27cec9410dcbd5ae708a065970694f2dc4691
Levi Jones <leviwill.j@gmail.com>
2023-11-14 10:38:03 -0700

    Wrote vec3 class and helped functions

--- vec3.h
+++ vec3.h
@@ -40,3 +40,56 @@ public:
     return sqrt(length_squared());
   }
 };
+
+// Make the class printable
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+  return out << v.x << ' ' << v.y << ' ' << v.z;
+}
+
+// Add operation
+inline vec3 operator+(const vec3& u, const vec3& v) {
+  return vec3(u.x + v.x, u.y + v.y, u.z + v.z);
+}
+
+// Substract operation
+inline vec3 operator-(const vec3& u, const vec3& v) {
+  return vec3(u.x - v.x, u.y - v.y, u.z - v.z);
+}
+
+// Multiply operation (vectors)
+inline vec3 operator*(const vec3& u, const vec3& v) {
+  return vec3(u.x * v.x, u.y * v.y, u.z * v.z);
+}
+
+// Multiply by scalar
+inline vec3 operator*(double t, const vec3& v) {
+  return vec3(t * v.x, t * v.y, t * v.z);
+}
+inline vec3 operator*(const vec3& v, double t) {
+  return t * v;
+}
+
+// Divide by scalar
+inline vec3 operator/(vec3& v, double t) {
+  return (1/t) * v;
+}
+
+// Dot product
+inline double dot(const vec3& u, const vec3& v) {
+  return u.x * v.x
+       + u.y * v.y
+       + u.z * v.z;
+}
+
+// Cross product
+inline vec3 cross(const vec3& u, const vec3& v) {
+  double x = u.y * v.z - u.z * v.y;
+  double y = u.z * v.x - u.x * v.z;
+  double z = u.x * v.y - u.y * v.x;
+  return vec3(x, y, z);
+}
+
+// Unit vector
+inline vec3 unit_vector(vec3 v) {
+  return v / v.length();
+}
